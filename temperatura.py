5f46d132f70be1569759d087a1f67cb534a424ce
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2023-03-05 20:44:29 -0300

    Create temperatura.py
    Criando um algoritmo com o chat GPT

---
+++ temperatura.py
@@ -0,0 +1,22 @@
+# Pergunta ao usuário qual a unidade de temperatura e o valor da temperatura
+unidade = input("Digite a unidade de temperatura (C, F, K): ")
+temperatura = float(input("Digite o valor da temperatura: "))
+
+# Converte a temperatura para Celsius
+if unidade == "F":
+    celsius = (temperatura - 32) * 5/9
+elif unidade == "K":
+    celsius = temperatura - 273.15
+else:
+    celsius = temperatura
+
+# Converte a temperatura para Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Converte a temperatura para Kelvin
+kelvin = celsius + 273.15
+
+# Exibe os resultados das conversões
+print("Celsius: ", celsius)
+print("Fahrenheit: ", fahrenheit)
+print("Kelvin: ", kelvin)
